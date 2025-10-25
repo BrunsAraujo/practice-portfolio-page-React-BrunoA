dd6d18751a4affa1730f664e9202d0190ab4521e
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-10-25 11:58:30 -0500

    In Class Group work

---
+++ using-state-in-forms/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import FeedBackForm from './Component/FeedBackForm2.jsx'
+
+import "./App.css";
+
+function App() {
+	return (
+		<div className="app">
+			<FeedBackForm />
+		</div>
+	);
+}
+export default App;
